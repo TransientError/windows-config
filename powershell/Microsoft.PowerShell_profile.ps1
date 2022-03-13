b08582fff16b4c1237c81d01cdb85935a7dd9bc6
Kevin Wu <wukevin@microsoft.com>
2022-03-13 03:25:28 -0700

    fix set-psreadlinekeyhandlers

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,11 +2,15 @@
 # https://gist.github.com/shanselman/25f5550ad186189e0e68916c6d7f44c3?WT.mc_id=-blog-scottha
 
 Invoke-Expression (&starship init powershell)
+Set-PsReadlineOption -predictionsource history
+Set-PsReadlineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-set-psreadlineoption -predictionsource history
-set-psreadlineoption -HistorySearchCursorMovesToEnd
-Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Chord Ctrl+r -ScriptBlock {
+  Get-Content (Get-PSReadlineoption).HistorySavePath | fzf | Invoke-expression
+}
 
 
 # PowerShell parameter completion shim for the dotnet CLI
@@ -31,10 +35,6 @@ if (Test-Path($solutionPackagerPath)) {
 }
 
 Set-Alias -Name start-komorebi -Value 'C:\Users\wukevin\utils\komorebic-scripts\start-komorebic.ps1'
-
-Set-PSReadLineKeyHandler -Chord Ctrl+r -ScriptBlock {
-  cat (Get-PSReadlineoption).HistorySavePath | fzf | Invoke-expression
-}
 
 Invoke-Expression (& { $hook = if ($PSVersionTable.PSVersion.Major -ge 6) { 'pwd' } else { 'prompt' } (zoxide init powershell --hook $hook | Out-String) })
 
