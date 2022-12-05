a7623e5cfd26eec32daf42ee65ae8dd59e27ac89
Kevin Wu <wukevin@microsoft.com>
2022-12-05 15:48:07 -0800

    devenv2019

--- cmder/user_aliases.cmd
+++ cmder/user_aliases.cmd
@@ -13,3 +13,4 @@ unalias=alias /d $1
 vi=vim $*
 cmderr=cd /d "%CMDER_ROOT%"
 devenv="C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
+devenv2019="C:\Program Files\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
