aa552db47937151652c359dd5e6cabd4dae23e73
Kevin Wu <wukevin@microsoft.com>
2023-02-24 08:45:52 -0800

    some fixes

--- komorebi/komorebi.ps1
+++ komorebi/komorebi.ps1
@@ -13,6 +13,10 @@ komorebic toggle-cross-monitor-move-behaviour
 komorebic float-rule class TaskManagerWindow
 komorebic float-rule title "Microsoft Teams Notification"
 
+# Visual studio
+komorebic identify-object-name-change-application exe "devenv.exe"
+komorebic float-rule class "WindowsForms10.Window.8.app.0.2b7bf41_r21_ad1"
+
 # If my particular ultrawide monitor prefer columns
 if (((Get-WmiObject WmiMonitorID -namespace root\wmi).InstanceName) -like "*VTK*") {
    # try ultrawide-vertical-stack
