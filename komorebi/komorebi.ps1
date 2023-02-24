1b47b2e4e858b60c1dd44ec20e957775cc3e81ea
Kevin Wu <wukevin@microsoft.com>
2023-02-24 08:00:51 -0800

    new komorebic configuration framework

---
+++ komorebi/komorebi.ps1
@@ -0,0 +1,22 @@
+if (!(Get-Process whkd -ErrorAction SilentlyContinue)) {
+  Start-Process whkd -WindowStyle hidden
+}
+
+komorebic watch-configuration enable
+komorebic focus-follows-mouse enable
+
+komorebic active-window-border-colour 142 140 216
+komorebic active-window-border enable
+komorebic toggle-cross-monitor-move-behaviour
+
+# rules
+komorebic float-rule class TaskManagerWindow
+komorebic float-rule title "Microsoft Teams Notification"
+
+# If my particular ultrawide monitor prefer columns
+if (((Get-WmiObject WmiMonitorID -namespace root\wmi).InstanceName) -like "*VTK*") {
+   # try ultrawide-vertical-stack
+   komorebic workspace-layout 0 0 columns
+}
+
+komorebic complete-configuration
