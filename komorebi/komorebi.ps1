1a5ecc59530ff83466f60c205244f4ec3e9e004d
Kevin Wu <wukevin@microsoft.com>
2023-05-24 15:29:18 -0700

    new windows settings for komorebi

--- komorebi/komorebi.ps1
+++ komorebi/komorebi.ps1
@@ -16,9 +16,11 @@ komorebic float-rule title "Microsoft Teams Notification"
 # Visual studio
 komorebic identify-object-name-change-application exe "devenv.exe"
 komorebic float-rule title "WindowsFormsParkingWindow"
+komorebic float-rule title "Search everywhere"
 
 # preview teams
 komorebic identify-object-name-change-application exe "ms-teams.exe"
+komorebic identify-tray-application exe ms-teams.exe
 
 # If my particular ultrawide monitor prefer columns
 if (((Get-WmiObject WmiMonitorID -namespace root\wmi).InstanceName) -like "*VTK*") {
