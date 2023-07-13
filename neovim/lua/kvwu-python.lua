20f1293c972ea98393ccfaedb73d15ec4eeef8c8
Kevin Wu <wukevin@microsoft.com>
2023-07-13 02:01:23 -0700

    reintroduce rainbow indent blankline for yaml

--- neovim/lua/kvwu-python.lua
+++ neovim/lua/kvwu-python.lua
@@ -22,11 +22,26 @@ function kvwu_python.setup(use, not_vscode)
   use {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
+      vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
       require("indent_blankline").setup {
-        show_current_context = true,
+        show_char_blankline = " ",
+        char_highlight_list = {
+          "IndentBlanklineIndent1",
+          "IndentBlanklineIndent2",
+          "IndentBlanklineIndent3",
+          "IndentBlanklineIndent4",
+          "IndentBlanklineIndent5",
+          "IndentBlanklineIndent6",
+        },
       }
     end,
-    ft = {"python", "yaml"},
+    ft = { "python", "yaml" },
     cond = not_vscode,
   }
   use "michaeljsmith/vim-indent-object"
