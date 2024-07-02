f564b5d28314cb927a53229ec50a7c57048fb823
Kevin Wu <wukevin@microsoft.com>
2024-07-02 00:34:37 -0700

    indent blankline new api

--- neovim/lua/kvwu-python.lua
+++ neovim/lua/kvwu-python.lua
@@ -22,24 +22,25 @@ function kvwu_python.setup(use, not_vscode)
   use {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
-      vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-
-      require("indent_blankline").setup {
-        show_char_blankline = " ",
-        char_highlight_list = {
-          "IndentBlanklineIndent1",
-          "IndentBlanklineIndent2",
-          "IndentBlanklineIndent3",
-          "IndentBlanklineIndent4",
-          "IndentBlanklineIndent5",
-          "IndentBlanklineIndent6",
-        },
+      local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowGreen",
+        "RainbowBlue",
+        "RainbowCyan",
+        "RainbowViolet",
       }
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function ()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      end)
+
+      require("ibl").setup { indent = { highlight = highlight } }
     end,
     ft = { "python", "yaml" },
     cond = not_vscode,
