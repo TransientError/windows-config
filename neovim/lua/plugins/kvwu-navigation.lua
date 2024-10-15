e7002e9d5b4513b874c9fa1675d77a966d840829
kvwu <kvwu@transienterror.com>
2024-10-15 01:34:07 -0700

    add multi cursor

--- neovim/lua/plugins/kvwu-navigation.lua
+++ neovim/lua/plugins/kvwu-navigation.lua
@@ -26,6 +26,22 @@ return {
     cmd = { "NvimTreeToggle" },
     keys = "<leader>op",
     ft = ""
-  }
+  },
+  {
+    "brenton-leighton/multiple-cursors.nvim",
+    opts = {}, -- This causes the plugin setup function to be called
+    keys = {
+      { "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" }, desc = "Add cursor and move down" },
+      { "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" }, desc = "Add cursor and move up" },
+
+      { "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
+      { "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
+
+      { "<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" }, desc = "Add or remove cursor" },
+
+      { "<M-d>", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Jump to next cword" },
+
+    },
+  },
 }
 
