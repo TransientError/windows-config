3bfa50b0d3e4c784ff7ff8c67f6c4a5d91481f54
Kevin Wu <wukevin@microsoft.com>
2025-05-12 22:23:35 -0700

    wezterm: restore minimize button on windows

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,11 +2,14 @@ local wezterm = require "wezterm" --[[@as Wezterm]]
 local config = wezterm.config_builder()
 
 local tabline = wezterm.plugin.require "https://github.com/michaelbrusegard/tabline.wez"
+tabline.setup { options = { theme = 'Tomorrow Night Bright' } }
+tabline.apply_to_config(config)
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.default_prog = { "pwsh" }
   config.color_scheme = "Campbell (Gogh)"
   config.prefer_egl = true
+  config.window_decorations = "TITLE | RESIZE"
 end
 
 config.font = wezterm.font "LigaHack Nerd Font"
@@ -65,7 +68,5 @@ config.keys = {
   },
 }
 
-tabline.setup { options = { theme = 'Tomorrow Night Bright' } }
-tabline.apply_to_config(config)
 config.tab_bar_at_bottom = true
 return config
