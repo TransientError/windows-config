4ac10c6660759a40c3038e7bbab8a964e4d0084e
Kevin Wu <kvwu@transienterror.com>
2025-05-26 22:28:09 -0700

    nvim: treesitter-textobjects

--- AppData/Local/nvim/lua/plugins/kvwu-treesitter.lua
+++ AppData/Local/nvim/lua/plugins/kvwu-treesitter.lua
@@ -18,13 +18,77 @@ return {
 
           if vim.treesitter.language.add(lang) then
             vim.treesitter.start(args.buf, lang)
-          else
-            error(string.format("treesitter not installed for %s", lang))
           end
         end,
       })
 
       vim.treesitter.language.register("powershell", "ps1")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    lazy = false,
+    branch = "main",
+    opts = {
+      move = {
+        set_jumps = true,
+      },
+    },
+    keys = function()
+      local move = require "nvim-treesitter-textobjects.move"
+      local swap = require "nvim-treesitter-textobjects.swap"
+      ---@type LazyKeysSpec[]
+      local move_mappings = {
+        {
+          "]f",
+          function()
+            move.goto_next_start("@function.outer", "textobjects")
+          end,
+        },
+        {
+          "]c",
+          function()
+            move.goto_next_start("@class.outer", "textobjects")
+          end,
+        },
+        {
+          "[f",
+          function()
+            move.goto_previous_start("@function.outer", "textobjects")
+          end,
+        },
+        {
+          "[c",
+          function()
+            move.goto_previous_start("@class.outer", "textobjects")
+          end,
+        },
+      }
+
+      local swap_mappings = {
+        {
+          "<leader>a",
+          function()
+            swap.swap_next("@parameter.inner", "textobjects")
+          end,
+        },
+        {
+          "<leader>A",
+          function()
+            swap.swap_previous("@parameter.outer", "textobjects")
+          end,
+        },
+      }
+
+      for _, mapping in ipairs(move_mappings) do
+        mapping.mode = { "n", "x", "o" }
+      end
+
+      for _, mapping in ipairs(swap_mappings) do
+        mapping.mode = { "n" }
+      end
+
+      return utils.concat(move_mappings, swap_mappings)
     end,
   },
 }
--- AppData/Local/nvim/lua/utils.lua
+++ AppData/Local/nvim/lua/utils.lua
@@ -21,4 +21,34 @@ function utils.not_vscode()
   return fn.exists "g:vscode" == 0
 end
 
+---@generic T
+---@param ... (T[]|T)[] 
+---@return T[]
+function utils.concat(...)
+  local t = {}
+  for _, v in ipairs { ... } do
+    if type(v) == "table" then
+      for _, v2 in ipairs(v) do
+        table.insert(t, v2)
+      end
+    else
+      table.insert(t, v)
+    end
+  end
+  return t
+end
+
+---@generic K, V
+---@param ... table<K, V>[]
+---@return table<K, V>
+function utils.merge(...)
+  local t = {}
+  for _, v in ipairs { ... } do
+    for k, v2 in pairs(v) do
+      t[k] = v2
+    end
+  end
+  return t
+end
+
 return utils
