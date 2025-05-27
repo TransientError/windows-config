2b56d949a5bffd454ab821498e87f2fa9f29aeec
Kevin Wu <kvwu@transienterror.com>
2025-05-27 02:14:01 -0700

    wezterm: factor out keys

---
+++ .config/wezterm/keys.lua
@@ -0,0 +1,71 @@
+local wezterm = require "wezterm"
+local M = {}
+
+function M.apply_to_config(config)
+  config.leader = { key = ",", mods = "CTRL" }
+
+  config.keys = {
+    {
+      key = "c",
+      mods = "CTRL",
+      action = wezterm.action_callback(function(window, pane)
+        if pane == nil then
+          wezterm.log_error "pane is nil"
+          return
+        end
+        local selection_text = window:get_selection_text_for_pane(pane)
+        local is_selection_active = string.len(selection_text) ~= 0
+        if is_selection_active then
+          window:perform_action(wezterm.action.CopyTo "ClipboardAndPrimarySelection", pane)
+        else
+          window:perform_action(wezterm.action.SendKey { key = "c", mods = "CTRL" }, pane)
+        end
+      end),
+    },
+    {
+      key = "l",
+      mods = "LEADER",
+      action = wezterm.action.ShowLauncher,
+    },
+    {
+      key = "s",
+      mods = "LEADER",
+      action = wezterm.action.QuickSelect,
+    },
+    {
+      key = "c",
+      mods = "LEADER",
+      action = wezterm.action.ActivateCopyMode,
+    },
+    {
+      key = "j",
+      mods = "LEADER",
+      action = wezterm.action.ActivateCommandPalette,
+    },
+    {
+      key = "v",
+      mods = "CTRL",
+      action = wezterm.action.PasteFrom "Clipboard",
+    },
+    {
+      key = "w",
+      mods = "CTRL",
+      action = wezterm.action.CloseCurrentTab { confirm = true },
+    },
+    {
+      key = "t",
+      mods = "CTRL",
+      action = wezterm.action.SpawnTab "CurrentPaneDomain",
+    },
+  }
+
+  for i = 0, 9, 1 do
+    table.insert(config.keys, {
+      key = tostring(i),
+      mods = "LEADER",
+      action = wezterm.action.ActivateTab((i - 1) % 10),
+    })
+  end
+end
+
+return M
--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ tabline.apply_to_config(config)
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.default_prog = { "pwsh" }
-  config.color_scheme = "Campbell (Gogh)"
+  config.color_scheme = "PaulMillr"
   config.prefer_egl = true
   config.window_decorations = "TITLE | RESIZE"
 end
@@ -25,57 +25,8 @@ config.launch_menu = {
   },
 }
 
-config.leader = { key = ",", mods = "CTRL" }
-config.keys = {
-  {
-    key = "c",
-    mods = "CTRL",
-    action = wezterm.action_callback(function(window, pane)
-      local selection_text = window:get_selection_text_for_pane(pane)
-      is_selection_active = string.len(selection_text) ~= 0
-      if is_selection_active then
-        window:perform_action(wezterm.action.CopyTo "ClipboardAndPrimarySelection", pane)
-      else
-        window:perform_action(wezterm.action.SendKey { key = "c", mods = "CTRL" }, pane)
-      end
-    end),
-  },
-  {
-    key = "l",
-    mods = "LEADER",
-    action = wezterm.action.ShowLauncher,
-  },
-  {
-    key = "s",
-    mods = "LEADER",
-    action = wezterm.action.QuickSelect,
-  },
-  {
-    key = "c",
-    mods = "LEADER",
-    action = wezterm.action.ActivateCopyMode,
-  },
-  {
-    key = "j",
-    mods = "LEADER",
-    action = wezterm.action.ActivateCommandPalette,
-  },
-  {
-    key = "v",
-    mods = "CTRL",
-    action = wezterm.action.PasteFrom "Clipboard",
-  },
-  {
-    key = "w",
-    mods = "CTRL",
-    action = wezterm.action.CloseCurrentTab { confirm = true },
-  },
-  {
-    key = "t",
-    mods = "CTRL",
-    action = wezterm.action.SpawnTab "CurrentPaneDomain",
-  },
-}
+local keys = require("keys")
+keys.apply_to_config(config)
 
 config.tab_bar_at_bottom = true
 return config
