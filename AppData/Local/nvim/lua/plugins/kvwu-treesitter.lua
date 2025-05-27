db515e712674b543e3744d9a91a013ad0b161f72
Kevin Wu <kvwu@transienterror.com>
2025-05-26 21:00:44 -0700

    nvim: upgrade treesitter

--- AppData/Local/nvim/lua/plugins/kvwu-treesitter.lua
+++ AppData/Local/nvim/lua/plugins/kvwu-treesitter.lua
@@ -2,18 +2,29 @@ local utils = require "utils"
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     cond = utils.not_vscode,
-    opts = {
-      ensure_installed = { "lua" },
-      sync_install = false,
-      auto_install = false,
-      ignore_install = {},
-      modules = {},
-      highlight = {
-        enable = true,
-      },
-    },
-    main = "nvim-treesitter.configs",
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "*",
+        callback = function(args)
+          local ft = vim.bo.filetype
+          local lang = vim.treesitter.language.get_lang(ft)
+          if lang == nil then
+            return
+          end
+
+          if vim.treesitter.language.add(lang) then
+            vim.treesitter.start(args.buf, lang)
+          else
+            error(string.format("treesitter not installed for %s", lang))
+          end
+        end,
+      })
+
+      vim.treesitter.language.register("powershell", "ps1")
+    end,
   },
 }
