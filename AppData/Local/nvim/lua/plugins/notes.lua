b0df888e6bfdf8b1756c1b021a21e1f7edf09b15
Kevin Wu <kvwu@transienterror.com>
2025-07-28 17:10:42 -0700

    nvim: neowiki

--- AppData/Local/nvim/lua/plugins/notes.lua
+++ AppData/Local/nvim/lua/plugins/notes.lua
@@ -6,4 +6,20 @@ return {
       vim.g.table_mode_map_prefix = "<localleader>t"
     end,
   },
+  {
+    "echaya/neowiki.nvim",
+    opts = {
+      wiki_dirs = {
+        { name = "notes", path = "~/OneDrive/Documents/neowiki" },
+      },
+    },
+    keys = function ()
+      local neowiki = require("neowiki")
+      return {
+        { "<leader>ww", neowiki.open_wiki, desc = "open wiki" },
+        { "<leader>wW", neowiki.open_wiki_floating, desc = "open floating" },
+        { "<leader>wt", neowiki.open_wiki_new_tab, desc = "wiki new tab" },
+      }
+    end
+  },
 }
