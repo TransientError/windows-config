a7ab2fab034bbc4df6e5816d64846e417def5c6c
Kevin Wu <kvwu@transienterror.com>
2025-12-29 19:40:32 -0800

    frecency

--- AppData/Local/nvim/lua/plugins/kvwu-telescope.lua
+++ AppData/Local/nvim/lua/plugins/kvwu-telescope.lua
@@ -10,13 +10,17 @@ return {
       "nvim-telescope/telescope-fzf-native.nvim",
       "nvim-telescope/telescope-live-grep-args.nvim",
       "nvim-telescope/telescope-project.nvim",
+      {
+        "nvim-telescope/telescope-frecency.nvim",
+        version = "*",
+      },
     },
     keys = function()
       local builtin = require "telescope.builtin"
       local telescope = require "telescope"
       return {
         { "<leader>pf", builtin.find_files },
-        { "<leader>fr", builtin.oldfiles },
+        { "<leader>fr", telescope.extensions.frecency.frecency },
         { "<leader>,", builtin.buffers },
         { "<leader>ha", builtin.help_tags },
         { "<leader>/", telescope.extensions.live_grep_args.live_grep_args },
@@ -27,14 +31,21 @@ return {
       local telescope = require "telescope"
       telescope.load_extension "live_grep_args"
       telescope.load_extension "project"
+      telescope.load_extension "frecency"
+      vim.keymap.set("n", "<leader>pf", function()
+        telescope.extensions.frecency.frecency {
+          workspace = "CWD",
+          path_display = { "shorten" },
+        }
+      end)
 
       telescope.setup {
         defaults = {
           mappings = {
             n = {
-              ["q"] = require("telescope.actions").close
-            }
-          }
+              ["q"] = require("telescope.actions").close,
+            },
+          },
         },
         extensions = {
           live_grep_args = {
